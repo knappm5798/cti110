0c896483dc0982b9b785fb8c9ecfaebb8057da91
knappm5798 <67263929+knappm5798@users.noreply.github.com>
2020-06-22 12:32:58 +0300

    Add files via upload
    Calculations

---
+++ P2HW1_BasicMath_MichaelKnapp.py
@@ -0,0 +1,12 @@
+First_number= int(input('What is the first number? '))
+Second_number= int(input('What is the second number? '))
+add= First_number + Second_number
+multiply= First_number * Second_number
+
+print ('Your first number: ', First_number)
+print ('Your Second number: ', Second_number)
+print ('Your numbers added: ', add)
+print ('Your numbers multiplied: ', multiply)
+
+
+
