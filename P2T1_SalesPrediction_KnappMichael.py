8c88df056fbff7cae46f513cd2db06ad4c5e0184
knappm5798 <67263929+knappm5798@users.noreply.github.com>
2020-06-22 12:28:45 +0300

    Add files via upload
    Tutorial on sales prediction using calculations

---
+++ P2T1_SalesPrediction_KnappMichael.py
@@ -0,0 +1,11 @@
+# Sales Prediction
+# June 23 2020
+# CTI-110 P2T1 - Sales Prediction
+# Michael Knapp
+#
+#Ask for amount of sales
+total_sales= float(input('What are the total projected sales? '))
+#Calculate input from user
+profit=total_sales * 0.23
+#display calculation to user
+print('Total projected Profits: ',profit)
