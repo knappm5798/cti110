898a62586f0083d1ee2b55254dec7186f5ff5ce6
knappm5798 <67263929+knappm5798@users.noreply.github.com>
2020-06-22 12:33:55 +0300

    Add files via upload
    Using turtle in python

---
+++ P2HW2_TurtleGraphic_Knapp,Michael.py
@@ -0,0 +1,30 @@
+# Turtle Graphic
+# June 23 2020
+# CTI-110 P2HW2 - Turtle Graphic
+# Michael Knapp
+#Start Turtle
+import turtle
+turtle.showturtle()
+#Begin Drawing directions
+turtle.forward(100)
+turtle.forward(100)
+turtle.write('East')
+turtle.left(180)
+turtle.forward(400)
+turtle.write('West')
+turtle.left(180)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.write('North')
+turtle.left(180)
+turtle.forward(400)
+turtle.write('South')
+#Set turtle back to center
+turtle.left(180)
+turtle.forward(200)
+#Draw Circle
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.circle(50)
