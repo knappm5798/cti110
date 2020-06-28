50101da6ac3568ba137d7626ebc50f107c2cbfe1
knappm5798 <67263929+knappm5798@users.noreply.github.com>
2020-06-28 23:07:37 +0300

    Add files via upload
    Debug (indentation, alignment)

---
+++ P3LAB_Debugging_MichaelKnapp.py
@@ -0,0 +1,21 @@
+# CTI-110
+# P3LAB - Debugging
+# Michael Knapp
+# June 28 2020
+#
+#if then statements
+if score >=A_Score:
+    print('Your grade is A. ')
+else:
+    if score >=B_Score:
+        print('Your grade is B. ')
+    else:
+        if score >=C_Score:
+            print('Your grade is C. ')
+        else:
+            if score >=D_Score:
+                print('Your grade is D. ')
+            else:
+                if score >=F_Score:
+                    print('Your grade is F. ')
+                    
